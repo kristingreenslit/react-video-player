080b6f82d04243fcd1f420cc16caef9ab6158f9b
Kristin Greenslit <kristingreenslit@gmail.com>
2017-01-16 16:16:24 -0500

    refactor searchbar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,16 @@
-import React from 'react';
+// import React and pull off 'Component' as a variable called 'Component'
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />
-};
+// const SearchBar = () => {
+// 	return <input />
+// };
+
+// upgraded 'functional' component to 'class' component for added functionality
+class SearchBar extends Component {
+	render() {
+		return <input onChange={event => console.log(event.target.value)} />;
+	}
+}
 
 // only code exported out of this file is the SearchBar component
 export default SearchBar;
